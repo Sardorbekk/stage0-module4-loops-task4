e0615f8242472de90237523b6b74e0606906069b
Sardorbekk <sardorbekimomaddinov@gmail.com>
2023-02-08 10:43:13 +0500

    HI

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -3,5 +3,23 @@ package school.mjc.stage0.loops.task4;
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
 
-    }
-}
+
+        if (toBreakWith<numberToGoUntil){
+        for (int i=1;i<=numberToGoUntil;i++) {
+            System.out.println(i);
+            if (toBreakWith == i) {
+                break;
+            }}
+        }
+        else {
+            for (int i = 0; i <= numberToGoUntil; i++) {
+                if (i!=0) System.out.println(i);
+                else System.out.println("iterating till the end");
+                if (toBreakWith == i) {
+                    break;
+                }
+            }
+
+
+        }}}
+
--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,6 +2,27 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
+          int count=0;
+          int skipped=0;
+          if (numberToSkip>lastInRow)  System.out.println("number to skip is bugger then the last");
+          else if (lastInRow<0) System.out.println("last number in row is negative");
 
-    }
-}
+          else {
+                 for (int i = 0; i < lastInRow; i++) {
+                  if (i == numberToSkip) {
+                      continue;
+                  }
+                  count += i;
+                  if (i <= numberToSkip) {
+                      skipped += i;
+                  }
+                  if (numberToSkip == lastInRow) {
+                      count = 0;
+                  }
+              }
+              System.out.println("skipped sum is " + "" + (skipped + numberToSkip));
+              System.out.println("counted sum is " + count);
+
+          }
+
+    }}
